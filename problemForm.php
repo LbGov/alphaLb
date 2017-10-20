1f6e51595163d17314878094dc44385c0b9a1888
Ali Darwish <adarwish@omsar.gov.lb>
2017-10-20 09:22:12 +0300

    added problem form - php script - modified css

---
+++ problemForm.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="generator" content=
+"HTML Tidy for HTML5 for Windows version 5.2.0">
+<title></title>
+</head>
+
+
+<body>
+
+<?php
+$servername = "127.0.0.1:3306";
+$username = "root";
+$password = "dg@2017";
+$dbname = "feedback";
+$tablename="problem_table";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$doingAction=$_POST['doingAction'];
+$wrongAction=$_POST['wrongAction'];
+$URL = $_POST['Referer'];
+$submitButton=$_POST['action_doProcessProblem'];
+
+
+$insertQuery = "INSERT INTO problem_table
+          (doing_what, what_went_wrong,url)
+          VALUES
+          ('".$doingAction."', '".$wrongAction."', '".$URL."')";
+                          
+if ($conn->query($insertQuery) === TRUE) {
+	if($submitButton == "Submit"){
+	header( 'Location: http://portal.gov.lb/dg/browse/thank-you/index.html' );
+	}
+	else{
+	header( 'Location: http://portal.gov.lb/dg/browse/thank-you/index_ar.html' );
+	}
+} else {
+}
+
+?>
+
+</body>
+</html>
