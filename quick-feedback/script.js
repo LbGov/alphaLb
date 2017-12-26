d015f81bafb29abd26fd64cbe7750684b4bc95f4
Racha Kansoun <rkansoun@omsar.gov.lb>
2017-12-26 13:18:57 +0200

    quick feedback and pring division include in all pages

---
+++ quick-feedback/script.js
@@ -0,0 +1,19 @@
+var div = document.getElementById("wrong");
+var data = '  <div class="row" style="position:relative;left:20px;"><div class="dgDiv"><div class="PrintPageFeature page-feature"><div class="print-container">  <h2 class="sr-only">  Print</h2>';
+data=data+'<button type="button" class="btn-link" id="print-page-button"><i class="fa fa-print" aria-hidden="true"></i><span>Print</span>';
+data=data+'</button><noscript><div class="callout-alert">Sorry, this button doesn’t work without Javascript. Try pressing Control + P on your keyboard to print, or use your browser’s print option.</div></noscript>';
+
+data=data+'</div></div>  <a href="javascript:void(0)" onclick="showProblemForm()" style="color:darkslateblue"><b>Is There Something Wrong With This Page?</b></a><div>';
+
+data=data+'<form name="problemForm" action="scripts/problemForm.php" method="POST" onsubmit="return validateProblemForm();"><div id="problemFormFields" style="display:none;">&nbsp;<h2>Help us improve Gov.lb</h2>';
+
+data=data+'<label for="Form_FeedbackForm_Doing">What were you doing</label><input type="text" name="doingAction" class="dg-input-form" id="Form_FeedbackForm_Doing" required="required" /><label for="Form_FeedbackForm_Wrong">What went wrong</label><input type="text" name="wrongAction" class="dg-input-form" id="Form_FeedbackForm_Wrong" required="required" />';
+data=data+'<input type="hidden" name="Referer" value="http://Gov.lb/dg/index.html" class="hidden" id="Form_FeedbackForm_Referer" />&nbsp;<div><input type="submit" name="action_doProcessProblem" value="Submit" />';
+    
+data=data+'</div></div></form></div></div></div><br />';  
+            
+div.innerHTML = data;
+
+
+
+  
---
+++ quick-feedback/script_ar.js
@@ -0,0 +1,19 @@
+var div = document.getElementById("wrong");
+var data = ' <div class="row" dir="rtl" align="right" style="position:relative;right:10px;> <div class="dgDiv_ar"><div class="PrintPageFeature page-feature"> <div class="print-container" dir="rtl">';
+
+data = data + '<h2 class="sr-only">طباعة </h2><button type="button" class="btn-link" id="print-page-button">  <i class="fa fa-print" aria-hidden="true"></i>&nbsp;<span>طباعة</span>';
+data=data+'</button><noscript><div class="callout-alert">Sorry, this button doesn’t work without Javascript. Try pressing Control + P on your keyboard to print, or use your browser’s print option.</div></noscript> </div>';
+data=data+'</div> <a href="javascript:void(0)" onclick="showProblemForm() " style="color:darkslateblue"><b> هل هنالك مشكلة على الصفحة؟</b></a><div>';
+data=data+' <form name="problemForm" action="scripts/problemForm.php" method="POST" onsubmit="return validate ()"><div id="problemFormFields" style="display:none;" dir="rtl"> &nbsp;';
+data=data+'<h2> ساعدنا على تحسين GOV.LB</h2><label for="Form_FeedbackForm_Doing">ماذا كنت تفعل</label><input type="text" name="doingAction" class="dg_input_form_ar" id="Form_FeedbackForm_Doing" required="required" />';
+
+data=data+'<label for="Form_FeedbackForm_Wrong">ماذا حصل</label><input type="text" name="wrongAction" class="dg_input_form_ar" id="Form_FeedbackForm_Wrong" required="required" />';
+
+data=data+'<input type="hidden" name="Referer" value="http://Gov.lb/" class="hidden" id="Form_FeedbackForm_Referer" />&nbsp;<div>';
+data=data+' <input type="submit" name="action_doProcessProblem" value="إرسال" /></div></div></form></div></div></div>';
+
+   
+
+
+
+div.innerHTML = data;
