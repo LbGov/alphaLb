1f5a8519e4953113a4245e66cab2b104eedb5ffc
Racha Kansoun <rkansoun@omsar.gov.lb>
2017-12-28 09:27:01 +0200

    wrong div correction

--- quick-feedback/script_ar.js
+++ quick-feedback/script_ar.js
@@ -5,9 +5,9 @@ data = data + '<h2 class="sr-only">طباعة </h2><button type="button" class="
 data=data+'</button><noscript><div class="callout-alert">Sorry, this button doesn’t work without Javascript. Try pressing Control + P on your keyboard to print, or use your browser’s print option.</div></noscript> </div>';
 data=data+'</div> <a href="javascript:void(0)" onclick="showProblemForm() " style="color:darkslateblue"><b> هل هنالك مشكلة على الصفحة؟</b></a><div>';
 data=data+' <form name="problemForm" action="scripts/problemForm.php" method="POST" onsubmit="return validate ()"><div id="problemFormFields" style="display:none;" dir="rtl"> &nbsp;';
-data=data+'<h2> ساعدنا على تحسين GOV.LB</h2><label for="Form_FeedbackForm_Doing">ماذا كنت تفعل</label><input type="text" name="doingAction" class="dg_input_form_ar" id="Form_FeedbackForm_Doing" required="required" />';
+data = data + '<h2> ساعدنا على تحسين GOV.LB</h2><label for="Form_FeedbackForm_Doing">ماذا كنت تفعل</label><br><input type="text" name="doingAction" class="dg_input_form_ar"  style="width:500px" id="Form_FeedbackForm_Doing" required="required" />';
 
-data=data+'<label for="Form_FeedbackForm_Wrong">ماذا حصل</label><input type="text" name="wrongAction" class="dg_input_form_ar" id="Form_FeedbackForm_Wrong" required="required" />';
+data = data + '<br><label for="Form_FeedbackForm_Wrong">ماذا حصل</label><br><input type="text" name="wrongAction" class="dg_input_form_ar" id="Form_FeedbackForm_Wrong" required="required" style="width:500px" /><br>';
 
 data=data+'<input type="hidden" name="Referer" value="http://Gov.lb/" class="hidden" id="Form_FeedbackForm_Referer" />&nbsp;<div>';
 data=data+' <input type="submit" name="action_doProcessProblem" value="إرسال" /></div></div></form></div></div></div>';
