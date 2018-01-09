d836677d488de6db4e99bbb4bf37b958f955eda3
Ali Darwish <adarwish@omsar.gov.lb>
2018-01-09 09:07:12 +0200

    bug fix in upload script

--- browse/work/hire-foreign-workers/foreign-workers/new-worker/category_4/prior-approval/online-application/upload.php
+++ browse/work/hire-foreign-workers/foreign-workers/new-worker/category_4/prior-approval/online-application/upload.php
@@ -22,7 +22,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     $uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpg"
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
